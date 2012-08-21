88f7fd022a10cc8cd03c63ae3455de6335eeca35
HyahhaProgrammer <postmaster@hyahha.info>
2012-08-22 00:20:35 +0900

    表示する時にURLをリンクに変換。 表示する時に改行を<br />に変換。

--- app/helpers/memos_helper.rb
+++ app/helpers/memos_helper.rb
@@ -1,2 +1,10 @@
+require 'uri'
+
 module MemosHelper
+  def replace_uri(s)
+    str = s.dup
+    uri_reg = URI.regexp(%w[http https])
+    str.gsub!(uri_reg) {%Q{<a href="#{$&}" target="_blank">#{$&}</a>}}
+    return str
+  end
 end
